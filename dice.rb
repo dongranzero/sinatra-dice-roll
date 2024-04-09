98660cb4ce7c8058aa1e252566b250a6b798a6f4
dongranzero <164931182+dongranzero@users.noreply.github.com>
2024-04-09 03:40:15 +0000

    color added

--- dice.rb
+++ dice.rb
@@ -25,23 +25,23 @@ end
 
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
+  @first_die = rand(1..6)
+	@second_die = rand(1..6)
+  @sum = @first_die + @second_die
 	
 	
-    @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+    @outcome = "You rolled a #{@first_die} and a #{@second_die} for a total of #{@sum}."
     erb(:two_six)
 	
 end
 
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die 
+  @first_die = rand(1..10)
+  @second_die = rand(1..10)
+  @sum = @first_die + @second_die 
 
-    @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+    @outcome = "You rolled a #{@first_die} and a #{@second_die} for a total of #{@sum}."
     erb(:two_ten)
  
 
